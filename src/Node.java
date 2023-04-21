b6d3ce07d10def83db5c1f2c4446983471e08a9a
LayZm0de <92876001+LayZm0de@users.noreply.github.com>
2023-04-21 13:01:22 +0800

    Comments

--- src/Node.java
+++ src/Node.java
@@ -1,19 +1,24 @@
 class Node {
+	// A linked list to keep track of the node's history
 	public LinkedList history = new LinkedList();
+	// The integer value stored in the node
 	private int data;
+	// The next node in the linked list
 	private Node next;
+	// The previous node in the linked list
 	private Node prev;
 
+	// Constructor for creating a new node with the given data
 	public Node(int data) {
 		this.data = data;
 		this.next = null;
 		this.prev = null;
 	}
-
+	// Getters and Setters
 	public int getData() {
 		return data;
 	}
-
+	
 	public void setData(int data) {
 		history.addNode(this.data);
 		this.data = data;
