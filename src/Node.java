f25b5aaa611c1b756325e2a2f12bdc53fd00e4ca
John Emmanuel Sales <66854142+JE-Sales@users.noreply.github.com>
2023-04-21 09:52:50 +0800

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,42 +1,37 @@
 class Node {
-	public LinkedList history = new LinkedList(); 
-    private int data;
-    private Node next;
-    private Node prev;
-    private boolean indentifier = false;
+	public LinkedList history = new LinkedList();
+	private int data;
+	private Node next;
+	private Node prev;
 
-    public Node(int data) {
-        this.data = data;
-        this.next = null;
-        this.prev = null;
-    }
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+		this.prev = null;
+	}
 
-    public int getData() {
-        return data;
-    }
+	public int getData() {
+		return data;
+	}
 
-    public void setData(int data) {
-    	if(!indentifier) {
-    		history.addNode(this.data);
-    		indentifier = true;
-    	}
-        history.addNode(data);
-    	this.data = data;
-    }
+	public void setData(int data) {
+		history.addNode(this.data);
+		this.data = data;
+	}
 
-    public Node getNext() {
-        return next;
-    }
+	public Node getNext() {
+		return next;
+	}
 
-    public void setNext(Node next) {
-        this.next = next;
-    }
+	public void setNext(Node next) {
+		this.next = next;
+	}
 
-    public Node getPrev() {
-        return prev;
-    }
+	public Node getPrev() {
+		return prev;
+	}
 
-    public void setPrev(Node prev) {
-        this.prev = prev;
-    }
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
 }
